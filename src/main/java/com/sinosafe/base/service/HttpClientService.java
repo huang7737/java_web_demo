f70d3474a3083c53ac4e1a276f94f47c6295dda6
huangping <huangping5@sinosafe.com.cn>
2017-06-12 10:36:54 +0800

    http client

---
+++ src/main/java/com/sinosafe/base/service/HttpClientService.java
@@ -0,0 +1,14 @@
+package com.sinosafe.base.service;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+
+public interface HttpClientService {
+	public CloseableHttpClient getConnection();
+	public String httpPost(String url, String reqData);
+	public String httpGet(String url);
+	public String httpGet(String url,Map<String,String> headers);
+	public String download(String url, String filepath) throws IOException;
+}
